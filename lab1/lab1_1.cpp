07eb6ff77b3463da87acc5c4919b9652a17c5b4d
Sergey <semenov_ss@edu.surgu.ru>
2026-09-13 16:54:37 +0500

    Реализуем функцию для вывода массива на экран, также добавляем комментарии с пояснениями

--- lab1/lab1_1.cpp
+++ lab1/lab1_1.cpp
@@ -3,10 +3,19 @@
 #include <ctime>
 using namespace std;
 
+// Заполнение массива рандом числами 
 void fillArray(int (&arr)[10]) {
     for (int i = 0; i < 10; i++) {
         arr[i] = rand() % 100;
     }
+}
+
+// Выводим на экран массив
+void printArray(const int(&arr)[10]) {
+    for (const auto& x : arr) {
+        cout << x << " ";
+    }
+    cout << endl;
 }
 
 int main() {
@@ -16,6 +25,8 @@ int main() {
     int arr[10];
     
     fillArray(arr);
+    cout << "Исходный массив: ";
+    printArray(arr);
 
     return 0;
 }

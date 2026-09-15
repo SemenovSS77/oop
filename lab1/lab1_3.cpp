7d2181246a7e7db8879b4cf9754a0a85637340dd
Sergey <semenov_ss@edu.surgu.ru>
2026-09-15 22:40:25 +0500

    Добавляем функцию createArray

--- lab1/lab1_3.cpp
+++ lab1/lab1_3.cpp
@@ -6,8 +6,20 @@ struct SafeArray {
     int size;
 };
 
+SafeArray createArray(int size) {
+    SafeArray arr;
+    arr.size = size;
+    arr.data = new int[size]{};
+    return arr;
+}
+
 int main() {
     setlocale(LC_ALL, ".UTF-8");
+
+    SafeArray myArr = createArray(5);
+
+    delete[] myArr.data;
+    myArr.data = nullptr;
 
     return 0;
 }

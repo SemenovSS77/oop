5e7eb25d50364604d5ac22c12cd2bc6933b61899
Sergey <semenov_ss@edu.surgu.ru>
2026-09-15 19:18:40 +0500

    Запрашиваем у пользователя ввести N

---
+++ lab1/lab1_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, ".UTF-8");
+
+    int N{};
+    cout << "Введите размер массива N: ";
+    cin >> N;
+
+    return 0;
+}

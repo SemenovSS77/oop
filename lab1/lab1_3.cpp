f18ef75eb1bef3de66e2c56206d64e6871a5106d
Sergey <semenov_ss@edu.surgu.ru>
2026-09-15 23:12:44 +0500

    Добавляем функцию с выводом удаленных элементов и обнулением новых

--- lab1/lab1_3.cpp
+++ lab1/lab1_3.cpp
@@ -33,6 +33,40 @@ void printSafe(const SafeArray& arr) {
     cout << endl;
 }
 
+void reSizeArray(SafeArray& arr, int M) {
+    int N = arr.size;
+
+    if (M == N) {
+        return;
+    }
+
+    int* newData = new int[M]{};
+
+    if (M < N) {
+        // 6.1 При уменьшении массива выводим удаленные элементы
+        cout << "Удалены элементы: ";
+        for (int i = M; i < N; i++) {
+            cout << arr.data[i] << " ";
+        }
+        cout << endl;
+
+        // Копируем только первые M
+        for (int i = 0; i < M; i++) {
+            newData[i] = arr.data[i];
+        }
+    }
+    else {
+        // 6.2 При увеличении массива новые элементы равны 0
+        for (int i = 0; i < N; i++) {
+            newData[i] = arr.data[i];
+        }
+    }
+
+    delete[] arr.data;
+    arr.data = newData;
+    arr.size = M;
+}
+
 int main() {
     setlocale(LC_ALL, ".UTF-8");
 
@@ -42,10 +76,15 @@ int main() {
     getElement(myArr, 2) = 999;
     getElement(myArr, 4) = 42;
 
+    cout << "Исходный массив: " << endl;
     printSafe(myArr);
 
-    // Проверка выхода за границы
-    getElement(myArr, 10) = 777;
+    cout << "\nУменьшаем с 5 до 3: " << endl;
+    reSizeArray(myArr, 3);
+    printSafe(myArr);
+
+    cout << "\nУвеличиваем с 3 до 7: " << endl;
+    reSizeArray(myArr, 7);
     printSafe(myArr);
 
     delete[] myArr.data;

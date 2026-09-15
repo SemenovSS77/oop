161ee589ea1ed548178ef8f26e1ac04fb08937f1
Sergey <semenov_ss@edu.surgu.ru>
2026-09-15 22:45:37 +0500

    Добавляем функцию для проверки границ

--- lab1/lab1_3.cpp
+++ lab1/lab1_3.cpp
@@ -13,6 +13,18 @@ SafeArray createArray(int size) {
     return arr;
 }
 
+int& getElement(SafeArray& arr, int index) {
+    // 3.1 Проверяеем границы
+    if (index < 0 || index >= arr.size) {
+        cout << "Ошибка. Индекс " << index << " вне диапозона [0, " << arr.size - 1 << "]" << endl;
+
+        static int dummy = 0;
+        return dummy;
+    }
+    // 3.2 Корректный индекс
+    return arr.data[index];
+}
+
 int main() {
     setlocale(LC_ALL, ".UTF-8");
 

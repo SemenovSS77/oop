7da064e1877874db8c91242504b6de8056f875cc
Sergey <semenov_ss@edu.surgu.ru>
2026-09-15 19:21:40 +0500

    Выделяем память для массива

--- lab1/lab1_2.cpp
+++ lab1/lab1_2.cpp
@@ -9,5 +9,17 @@ int main()
     cout << "Введите размер массива N: ";
     cin >> N;
 
+    // Выделяем память
+    int* arr{ new int[N]{} };
+
+    cout << "Введите " << N << " элементов массива: ";
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Освобождаем память
+    delete[] arr;
+    arr = nullptr;
+
     return 0;
 }

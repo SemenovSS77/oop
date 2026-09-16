4b2f874cafad4176fe692bf7fa22a0c0a3e961ee
Sergey <semenov_ss@edu.surgu.ru>
2026-09-16 17:59:02 +0500

    Добавляем функцию для заполнения

--- lab1/lab1_4.cpp
+++ lab1/lab1_4.cpp
@@ -6,6 +6,17 @@ int** allocateMatrix(int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         matrix[i] = new int[cols]{};
     }
+    return matrix;
+}
+
+void fillMatrix(int** matrix, int rows, int cols) {
+    cout << "Введите " << rows * cols << " элементов матрицы (" << rows << "x" << cols << "):\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "[" << i << "][" << j << "]: ";
+            cin >> matrix[i][j]; 
+        }
+    }
 }
 
 int main() {

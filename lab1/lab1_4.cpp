a255d3a6ba2121dbca95f139360cbb0de907984e
Sergey <semenov_ss@edu.surgu.ru>
2026-09-16 18:14:04 +0500

    Реализуем красивый вывод

--- lab1/lab1_4.cpp
+++ lab1/lab1_4.cpp
@@ -19,6 +19,40 @@ void fillMatrix(int** matrix, int rows, int cols) {
     }
 }
 
+void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, string title = "Matrix") {
+    cout << "\n ||| " << title << " |||\n";
+
+    if (showBorders) {
+        cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << "*****";
+        }
+        cout << "*\n";
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        if (showBorders) cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << " " << matrix[i][j];
+
+            int len = to_string(matrix[i][j]).length();
+            for (int k = len; k < 4; ++k) cout << " ";
+            if (showBorders) cout << "*";
+        }
+        if (showBorders) cout << "*";
+        cout << "\n";
+    }
+
+    if (showBorders) {
+        cout << "*";
+        for (int j = 0; j < cols; ++j) {
+            cout << "*****";
+        }
+        cout << "*\n";
+    }
+    cout << endl;
+}
+
 int main() {
     setlocale(LC_ALL, ".UTF-8");
     return 0;
